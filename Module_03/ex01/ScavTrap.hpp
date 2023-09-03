a64ab5a186f96d66924a0b07868c85b46c1c8322
Carlos Ortiz <carlosortiz@MacBook-Pro-de-Carlos.local>
2023-09-03 16:36:53 +0200

    fin ex01

---
+++ Module_03/ex01/ScavTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	private :
+
+	public :
+		void attack(const std::string &target);
+		void guardGate(void);
+
+		ScavTrap &operator=(const ScavTrap &rhs);
+
+		ScavTrap(void);
+		ScavTrap(const ScavTrap &copy);
+		ScavTrap(std::string name);
+		~ScavTrap(void);
+};
+
+#endif
